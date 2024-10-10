a02feec956d37e49c680d1acc11c7f9b2ecc8e4b
thanglv <thanglv@mirabo-global.com>
2024-10-10 18:49:10 +0700

    feat: fix error funcs

--- Assets/_Scripts/Enemies/Enemy.cs
+++ Assets/_Scripts/Enemies/Enemy.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //private Vector3 GenerateNavMeshPoint(Vector3 startPos, float radius)
+    //{
+    //    Vector2 dir = Random.insideUnitCircle * radius;
+    //    Vector3 nav
+    //    dir += startPos;
+    //    NavMeshHit hit;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //}
 }
